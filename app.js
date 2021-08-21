5dab5365c2b457ee432d6548e1dd77736f416cc4
Arunima Das <arunima.das.1314@gmail.com>
2021-08-21 15:40:44 +0530

    Level-2 authentication and security; Database encryption using mongoose-encrypt

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -19,6 +21,9 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
